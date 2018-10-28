278196cda5ba34b30395402abd1380f21cc6d715
Daniel Alkemic Czuba <alkemic7@gmail.com>
2018-10-28 08:15:38 +0100

    Added GetParam helper that returns single param

--- param.go
+++ param.go
@@ -7,6 +7,7 @@ import (
 
 var paramsKey = "_params_"
 
+// GetParams returns map of parameters parsed from url, will be empty in case if there were no params.
 func GetParams(r *http.Request) map[string]string {
 	params, _ := r.Context().Value(paramsKey).(map[string]string)
 	return params
@@ -24,4 +25,11 @@ func initParams(r *http.Request) {
 	ctx := r.Context()
 	ctx = context.WithValue(ctx, paramsKey, map[string]string{})
 	(*r) = *(r.WithContext(ctx))
+}
+
+// GetParam returns single parsed parameter from url, along with indicator if it was success.
+func GetParam(r *http.Request, key string) (string, bool) {
+	params := GetParams(r)
+	val, ok := params[key]
+	return val, ok
 }
